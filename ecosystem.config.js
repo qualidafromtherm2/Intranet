1e013f3a14293eefbc98c4d1e55538de36fdd4e0
Leandro Santos <qualidadefromtherm2@gmail.com>
2025-08-14 10:20:24 -0300

    somente para ver se vai afetar o sql

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,4 @@
+// ecosystem.config.js
 module.exports = {
   apps: [
     {
@@ -5,18 +6,33 @@ module.exports = {
       script: './watch_print.js',
       watch: true,
       env: {
-        PRINTER: 'ZebraZD220'     //  ← nome da fila do CUPS
+        PRINTER: 'ZebraZD220'
       }
     },
 
-    
     {
-  name: 'intranet_api',
-  script: './server.js',
-  watch: true,
-  ignore_watch: ['etiquetas', 'logs', 'node_modules'],
-  watch_delay: 500
-}
+      name: 'intranet_api',
+      script: './server.js',
+      watch: true,
+      ignore_watch: ['etiquetas', 'logs', 'node_modules'],
+      watch_delay: 500,
 
+      // padrão (sem indicar env) → usa 5001
+      env: {
+        PORT: 5001
+      },
+
+      // === MODO JSON (sem banco) ===
+      env_json: {
+        PORT: 5001
+        // intencionalmente sem DATABASE_URL
+      },
+
+      // === MODO POSTGRES (com banco) ===
+      env_pg: {
+        PORT: 5001
+        // A DATABASE_URL será injetada pelo comando (não colocamos aqui)
+      }
+    }
   ]
 };
